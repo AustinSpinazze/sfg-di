8ae1a66504c874cfe44dbd5ca8879ab2336ba996
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-23 14:00:50 -0500

    Added Java configuration for English, Spanish, and Primary greeting services

--- src/main/java/austinspinazze/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/austinspinazze/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package austinspinazze.springframework.sfgdi.config;
 
-import austinspinazze.springframework.sfgdi.services.ConstructorGreetingService;
-import austinspinazze.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import austinspinazze.springframework.sfgdi.services.SetterInjectedGreetingService;
+import austinspinazze.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
